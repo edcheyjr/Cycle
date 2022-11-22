62fd23aaea914f8743ad8bdda51e01c785bfc176
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-11-22 13:14:17 +0300

    updated connect.php
    added a first connect setup

--- connect.php
+++ connect.php
@@ -1,11 +1,16 @@
 <?php
+// configuration variables
 $host = "localhost";
 $user = "root";
-$password =null;
-$db =null;
+$password = null;
+$db ="ecycle";
+
+// used to run the fist time when the database is created ensure DO NOT delete and it is NULL!
+$first_db =null;
 
 
- 
+// first time run the database
+$first_connect =  mysqli_connect($host,$user,$password,$first_db);
+// this will the be used once the db is configured
 $connect = mysqli_connect($host,$user,$password,$db);
-
 
